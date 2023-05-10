05c446aee9d243e7aa41374b857e1366db462e07
PriyanshiGupt <priyanshigupt01@gmail.com>
2023-05-10 14:18:37 +0530

    integration changes

--- src/app/login/employee/employee.component.ts
+++ src/app/login/employee/employee.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CookieStorageService } from 'src/app/shared/services/cookie-storage.service';
+import { LoginService } from 'src/app/shared/services/login.service';
+import { ToasterService } from 'src/app/shared/services/toaster.service';
 
 @Component({
   selector: 'app-employee',
@@ -19,7 +21,9 @@ export class EmployeeComponent implements OnInit {
     private activatedRoute : ActivatedRoute,
     private cookieStorageService : CookieStorageService,
     private router : Router,
-    private formBuilder : FormBuilder
+    private formBuilder : FormBuilder,
+    private loginService : LoginService,
+    private toasterService : ToasterService
   ) { }
 
   ngOnInit(): void {
@@ -32,5 +36,21 @@ export class EmployeeComponent implements OnInit {
 
   navigateToEmployeePortal() {
     this.router.navigateByUrl('employee')
+  }
+
+  login() {
+    const loginDetails = this.loginForm.value
+    // console.log(loginDetails.password)
+    // loginDetails.password = this.loginService.hashPassword(loginDetails.password)
+    // console.log(loginDetails.password)
+    this.loginService.employeeLogin(loginDetails).subscribe((response : string) => {
+      console.log(response)
+      this.cookieStorageService.setCookie('employeeId', response)
+      this.toasterService.showSuccess("Login Success")
+      this.navigateToEmployeePortal()
+    }, err => {
+      console.log(err)
+      this.toasterService.showError(err.error)
+    })
   }
 }
---
+++ src/app/shared/services/login.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import * as bcrypt from 'bcryptjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private http : HttpClient
+  ) { }
+  
+  // hashPassword(password) {
+  //   return bcrypt.hashSync(password, 1)
+  // }
+  employeeLogin(data) {
+    return this.http.post(environment.baseUrl + 'Employee/login', data)
+  }
+}
